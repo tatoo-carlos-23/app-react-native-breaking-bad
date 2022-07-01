c77a676599c8eaa0e5ceea8f4aea059adb2d56bd
Carlos Abramonte <car.abr.04@gmail.com>
2022-07-01 09:41:35 -0500

    First change xd

--- App.tsx
+++ App.tsx
@@ -75,9 +75,10 @@ const App = () => {
           style={{
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
           }}>
-          <Section title="Step One">
+          <Section title="App breaking bad">
             Edit <Text style={styles.highlight}>App.tsx</Text> to change this
             screen and then come back to see your edits.
+
           </Section>
           <Section title="See Your Changes">
             <ReloadInstructions />
